c197313533f8a6636c8adf19ad6a44825d6d29a1
Anand Siddharth <anand.siddharth@hotmail.com>
2021-01-19 15:03:41 +0530

    update exports

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export { NeedsModeration } from "./interfaces/needsModeration";
 export { Revision } from "./model/revision";
 export { logit } from "./core/logit";
 export { ModeratePlugin };
+export { PendingApprovalError, DuplicatePendingApprovalError } from "./core/errors"
 export default ModeratePlugin;
